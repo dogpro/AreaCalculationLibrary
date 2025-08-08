d62e6751fe4100237a7cbca0c8d9e2fa5ea4d766
dogpro <31433965+dogpro@users.noreply.github.com>
2025-08-08 10:47:24 +0500

    Delete Question_2.sql

--- Question_2.sql
+++
@@ -1,4 +0,0 @@
-SELECT p.ProductName, ISNULL(c.CategoryName, 'No category') AS CategoryName
-FROM Products p
-LEFT JOIN ProductCategory pc ON p.ProductID = pc.ProductID
-LEFT JOIN Categories c ON pc.CategoryID = c.CategoryID;
